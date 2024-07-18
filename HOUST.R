449f9cb1dd57263d04e2f656821ee8e333c844ed
xbb <xbb66kw@gmail.com>
2024-07-18 10:01:32 +0800

    Add files via upload

--- HOUST.R
+++ HOUST.R
@@ -5,11 +5,17 @@
 # index finder
 index_finder <- function(x) { which(names(categories)  %in% x) }
 
+
+###
+####
+# data check
 # 302 x 126 matrix
 str(data_organized)
 date # 3/1/1999 - 4/1/2024
 categories
 names(categories)
+
+
 
 # Check if these FRED-MD series are included in our dataset
 "UEMP15T26" %in% names(categories)
@@ -20,6 +26,8 @@ names(categories)
 "UNRATE" %in% names(categories) 
 
 
+ 
+ 
 HOUST.ind <- index_finder("HOUST") # 48
 HOUST.friend.ind <- index_finder(c("HOUST", "HOUSTNE", "HOUSTMW", "HOUSTS", "HOUSTW"))
 
@@ -106,19 +114,61 @@ pca.regression.coef <- function(X, y, n_pc, ind_set = NULL) {
 ####
 #### Model fitter for pca.ar.model
 pca.ar.model.fitter <- function(X, y) {
-  coef <- pca.regression.coef(X, y, 3, ind_set = list(1:5, 6:10))
+  n_pc <- 4
+  coef <- pca.regression.coef(X, y, n_pc, ind_set = list(1:5, 6:10))
   predictor <- function(X.test) { cbind(1, X.test) %*% coef }
   return (predictor)
 }
 
 
 
-predictor <- pca.ar.model.fitter(X, y)
-# predicted values of y
+predictor <- pca.ar.model.fitter(X, response)
+# predicted values of response
 predictor(X)
 
 # validation fitted loss
 train_validation(pca.ar.model.fitter, X, response)
 # comparison with lm,.model
 train_validation(lm.model.fitter, X, response)
+
+
+
+###
+####
+# Your task:
+# pca AR(1), pca AR(2), pca AR(3), pca AR(4), 哪個好？ 
+# n_pc 多少比較好？
+# 如何造 AR(q) response 和 covaraince matrix?
+
+# Build an AR(q) model for "HOUST"
+response <- data_organized[, HOUST.ind]
+response <- response[(q + 1):length(response)]
+covaraince.matrix <- data_organized[, HOUST.friend.ind]
+# construct the design
+q <- 3
+X <- array(0, c(length(response), DIM(covaraince.matrix)[2] * q))
+for (t in 1:q) {
+  X[, ((t - 1) * DIM(covaraince.matrix)[2] + 1):(t * DIM(covaraince.matrix)[2])] <- 
+    covaraince.matrix[(q + 1 - t):(DIM(covaraince.matrix)[1] - t), ]
+}
+
+
+
+
+
+
+
+
+###
+####
+# Your final task!
+# 觀測資料的時間 (不一定要 1999 - 2024 全用), 變數選擇, lags數目選擇都會影響 factor model 的品質。
+# 對 regression model 亦是如此。
+# 要如何對兩種方法有一個公平的比較？
+
+# 平時習慣的統計工具都可以嘗試
+# 測試成果要能說服別人。
+# 可以互相解釋給成員聽，看看有沒有明顯不合理需要加強的步驟？
+
+# 歡迎提問，但最後還得靠自己呢！
 
